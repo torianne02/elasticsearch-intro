a5568502a450e9fa13db0f9ceb650313ddc5f3c4
torianne02 <victoria.fluharty@gmail.com>
2020-06-03 16:52:06 -0700

    continues following along with course makes changes to code via guided videos

--- client.js
+++ client.js
@@ -1,15 +1,17 @@
 const ELASTICSEARCH = require("elasticsearch");
 const client = new ELASTICSEARCH.Client({
-  host: "localhost:9200"
-  // log: "trace"
+  host: "localhost:9200",
+  apiVersion: '7.6'
 });
 
 client
   .get({
     index: "simpsons",
     type: "episode",
-    id: 999,
-    _sourceExclude: ""
+    id: 1,
+    _sourceIncludes: [
+      'title'
+    ]
   })
   .then(resp => {
     console.log(resp);
